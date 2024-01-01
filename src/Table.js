c64381166d30b0ec7e4e02576a85f721ed721d32
Ronald803 <ronaldblancobalboa@gmail.com>
2023-12-31 23:36:29 -0400

    table component created

---
+++ src/Table.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+function Table() {
+    const planes = [
+        {
+            manzano: 1,
+            lote: 1,
+            zona: "general",
+            url: "https://drive.google.com/file/d/1RDGMWnd2p-M45ftNJZMikyB0U0MRzaiJ/view?usp=drive_link"
+        },
+        {
+          manzano: 1,
+          lote: 2,
+          zona: "general",
+          url: "https://drive.google.com/file/d/1l5_7BOUcr_VjEWOW0A9eAKRUc0ZpNVaY/view?usp=sharing"
+        },
+        {
+          manzano: 1,
+          lote: 3,
+          zona: "general",
+          url: "https://drive.google.com/file/d/1Shqt3RZ_H1f97486TD3yK-71-dvjrjXP/view?usp=sharing"
+        },
+        {
+          manzano: 1,
+          lote: 4,
+          zona: "general",
+          url: "https://drive.google.com/file/d/1d7RdDtPGMukx-j_htsf8GUgm62bd5lhd/view?usp=sharing"
+        },
+        {
+          manzano: 1,
+          lote: 5,
+          zona: "general",
+          url: "https://drive.google.com/file/d/1tbR7OdzP1l2jmvXHwWWsQHtzeCuk5Dga/view?usp=sharing"
+        },
+        {
+          manzano: 1,
+          lote: 6,
+          zona: "general",
+          url: "https://drive.google.com/file/d/1ufdHdF5xxlpErvJ1ui_zgoAXLF8USW7w/view?usp=sharing"
+        },
+    ]
+  return (
+    <div>Table</div>
+  )
+}
+
+export default Table
